5ae65def8f2f65ff806cded9cbe419f127f57aef
Alexander671 <matveevalex97@yandex.ru>
2021-12-12 08:56:23 +0300

    Init autors

--- autors/models.py
+++ autors/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Autors(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank = True, null = True)
+    description = models.TextField()
+    class Meta:
+        verbose_name = ("Autor") # человекочитаемое имя объекта
+        verbose_name_plural = ("Autors")  #человекочитаемое множественное имя для Категорий
+    def __str__(self):
+        return self.name  # __str__ применяется для отображения объекта в интерфейсе
+
